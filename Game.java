9e742ccb1580f9e25e5d70477c31bb1d5dce314c
m-steph <86425576+m-steph@users.noreply.github.com>
2021-09-23 11:52:37 -0600

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,19 @@
+public class Game {
+
+  public static void main(String[] args) {
+
+  }
+  static Room[] createSimpleGame(int numKey){
+    return new Room[0];
+  }
+
+  static Room[] sortRooms(Room[] rooms){
+    return rooms;
+  }
+  static void printHallway(Room[] rooms, boolean[] keysFound){
+
+  }
+  static void printRoom(Room room, boolean[] keysFound){
+
+  }
+}
